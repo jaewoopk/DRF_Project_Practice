0463b9e4abf787532e5a2b519cadaf323f4c721d
jaewoopk <jaewoopk000@naver.com>
2023-04-26 19:38:15 +0900

    Day 3 Learn about serializer

--- HelloDRF/example/views.py
+++ HelloDRF/example/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets, permissions, generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404 # get_object_or_404 불러오기
+from .models import Book
+from .serializers import BookSerializer
 
 
 # --> FBV : Function Based View = 함수 기반 뷰
@@ -9,6 +12,32 @@ from rest_framework.decorators import api_view
 def HelloAPI(request) :
     return Response("hello world!")
 
-class HelloAPI(APIView) :
-    def get(self, request) :
-        return Response("hello wolrd")
+# class HelloAPI(APIView) :
+#     def get(self, request) :
+#         return Response("hello wolrd")
+
+@api_view(['GET','POST']) 
+def booksAPI(request) :
+    if request.method == 'GET' :
+        books = Book.objects.all() # Book 모델로부터 전체 데이터 가져오기
+        serializer = BookSerializer(books, many=True)
+        # 시리얼라이저에 전체 데이터를 한 번에 집어넣기 (직렬화, many=True)
+        return Response(serializer.data, stauts=status.HTTP_200_OK) # return Response
+    elif request.method == 'POST' : # POST 요청(도서 정보 등록)
+        serializer = BookSerializer(data=request.data)
+        # POST 요청으로 들어온 데이터를 시리얼라이저에 집어넣기
+        if serializer.is_valid() : # 유효한 데이터라면
+            serializer.save()
+            # 시리얼라이저의 역직렬화를 통해 save(), 모델 시리얼라이저의 기본 create() 함수가 동작
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            # 201 메시지를 보내며 성공!
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # 400 잘못된 요청!
+
+@api_view(['GET'])
+def bookAPI(request, bid) : # /book/bid
+    book = get_object_or_404(Book, bid=bid)
+    serializer = BookSerializer(book) # 시리얼라이저에 데이터를 집어넣기 (직렬화)
+    return Response(serializer.data, status=status.HTTP_200_OK) # return Response!
+
+
